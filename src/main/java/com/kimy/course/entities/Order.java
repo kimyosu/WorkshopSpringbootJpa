3550b77593707dfd506744ab358548b5f513d7ae
andrew <kimyosu@protonmail.com>
2025-10-08 00:56:49 -0300

    Order, instant, ISO 8601 - PART 1

---
+++ src/main/java/com/kimy/course/entities/Order.java
@@ -0,0 +1,77 @@
+package com.kimy.course.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Table(name = "tb_order") // nome da tabela no banco de dados
+@Entity // indica que a classe é uma entidade JPA
+public class Order implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto increment no MySQL
+    private Long id;
+    private Instant moment;
+
+    //******Cada Order precisa saber a qual User pertence, então a FK vai em Order (client_id).******
+    @ManyToOne // muitos para um com User
+    @JoinColumn(name = "client_id") // nome da chave estrangeira na tabela Order
+    // Um User pode fazer muitos pedidos (Orders) e um Order pode ter um User (client)
+    private User client;
+
+
+    public Order(){}
+
+    public Order(Long id, Instant moment, User client) {
+        this.id = id;
+        this.moment = moment;
+        this.client = client;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Objects.equals(id, order.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
--- src/main/java/com/kimy/course/entities/User.java
+++ src/main/java/com/kimy/course/entities/User.java
@@ -4,10 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 @Entity
 @Table(name="tb_user")
@@ -22,6 +26,11 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+    //Um User pode ter muitos Orders (pedidos).
+    //mappedBy = "client" indica que o lado dono do relacionamento é a classe Order, no atributo client.
+    @OneToMany(mappedBy = "client") // um para muitos com Order
+    //client é o nome do atributo na classe Order que referencia o User
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -72,6 +81,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
